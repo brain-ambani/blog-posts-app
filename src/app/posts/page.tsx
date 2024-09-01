33aaa475ceddc0367db5cfd191e11fd63fae0dd6
brain ambani <brainambani1@gmail.com>
2024-09-01 12:53:09 +0300

    added fetch function to the posts server component

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,31 @@
-export default function PostsPage() {
+import Link from "next/link";
+
+type Post = {
+  id: number;
+  title: string;
+};
+
+type Data = {
+  posts: Post[];
+};
+
+export default async function PostsPage() {
+  const response = await fetch("https://dummyjson.com/posts?limit=10");
+  const data: Data = await response.json();
+
   return (
-    <main className="text-center pt-32 px-5">
+    <main className="text-center pt-16 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">All posts</h1>
+
+      <ul>
+        {data.posts.map((post: Post) => (
+          <li key={post.id} className="text-left  mb-3">
+            <Link href={`/posts/${post.id}`} className="text-2xl ">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const navLinks = [
   {
@@ -13,6 +16,8 @@ const navLinks = [
 ];
 
 const Header = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <header className="flex justify-between items-center py-4 px-7 border-b">
       <Link href="/">
@@ -29,7 +34,12 @@ const Header = () => {
         <ul className="flex gap-x-5 text-[14px]">
           {navLinks.map((link) => (
             <li key={link.href}>
-              <Link className="text-zinc-400" href={link.href}>
+              <Link
+                className={` ${
+                  pathname === link.href ? "text-zinc-900" : "text-zinc-400"
+                }`}
+                href={link.href}
+              >
                 {link.label}
               </Link>
             </li>
